cd02e4a0e80f02b0b4972c7e17c27bf9099fa9de
leodlca <ldlcamorim@hotmail.com>
2017-10-30 11:04:41 -0200

    Corrigindo segundo evento

--- src/ratimbum_evento2.ino
+++ src/ratimbum_evento2.ino
@@ -14,38 +14,42 @@ void setup(){
   Serial.begin(9600);
   myStepper.setSpeed(60);
   servoMotor.attach(7);
-  servoMotor.write(90);
+  servoMotor.write(0);
   
 }
 
 void loop(){
   
   if(waitForSecondEvent) secondEventTrigger();
+  delay(50);
   
 }
 
 int verificaLuz(){
-  
+
+  Serial.println(analogRead(portaLDR));
   return analogRead(portaLDR);  
   
 }
 
 void secondEventTrigger(){
   
-  if(verificaLuz() > 500) return;
+  if(verificaLuz() < 900) return;
 
   waitForSecondEvent = false;
+
+  delay(500);
 
   myStepper.step(341); // ~60º sentido anti-horário (ABAIXA A PONTE).
 
   delay(1000);
 
-  servoMotor.write(0); // Libera bolinha.
+  servoMotor.write(90);
 
   delay(10000);
 
   // Volta servo e stepper ao normal depois de 10 segundos.
-  servoMotor.write(90);
+  servoMotor.write(0);
   myStepper.step(-341);
   
 }
